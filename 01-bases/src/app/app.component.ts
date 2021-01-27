cc60cf3ec6ba4d155ac8bdcb2e78320620e87346
3bort <78028202+3bort@users.noreply.github.com>
2021-01-27 22:12:17 +0100

    Crear componente manualmente
    Creación de un componente manual, crear el contador.component.ts con su decorador, añadir en app.module en la declaración del componente.

--- 01-bases/src/app/app.component.ts
+++ 01-bases/src/app/app.component.ts
@@ -7,23 +7,5 @@ import { Component } from '@angular/core';
   //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo: string = 'Contador App'; // El titulo que podemos colocar en app.component.html
-  numero: number = 10;
-  base: number = 5;
-
-  sumar() {
-    this.numero += 1 //Es necesario el this para apuntar a la instancia
-  }
-
-  restar() {
-    this.numero -= 1 //Es necesario el this para apuntar a la instancia
-  }
-
-  acumular( valor: number){
-    this.numero += valor;
-  }
-
-  aumentarCinco( valor2: number){
-    this.numero += valor2;
-  }
+  
 }
---
+++ 01-bases/src/app/contador.component.ts
@@ -0,0 +1,44 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-contador',
+    template: ` 
+    <h1>{{titulo}}</h1>
+
+    <h3>Aumentar de 1 en 1</h3>
+
+    <button (click) = "acumular(-1)">-1</button>
+    <span>{{numero}}</span>
+    <button (click) = "acumular(+1)">+1</button>
+
+    <h3>Aumentar de {{base}} en 5</h3>
+    <button (click) = "aumentarCinco(-base)">-{{base}}</button>
+    <span>{{numero2}}</span>
+    <button (click) = "aumentarCinco(base)">+{{base}}</button>
+`
+})
+export class ContadorComponent {
+
+    titulo: string = 'Contador App'; // El titulo que podemos colocar en app.component.html
+    numero: number = 0;
+    numero2: number = 0
+    base: number = 5;
+
+    sumar() {
+        this.numero += 1 //Es necesario el this para apuntar a la instancia
+    }
+
+    restar() {
+        this.numero -= 1 //Es necesario el this para apuntar a la instancia
+    }
+
+    acumular( valor: number){
+        this.numero += valor;
+    }
+
+    aumentarCinco( valor2: number){
+        this.numero2 += valor2;
+    }
+    
+}
