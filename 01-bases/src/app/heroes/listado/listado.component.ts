30e5130a7b11681208deec9d7459f25865bcaa0f
3bort <78028202+3bort@users.noreply.github.com>
2021-01-28 21:41:44 +0100

    Creación componente automaticamente
    Creación de componente con el comando en terminal -> ng g c heroes/listado

---
+++ 01-bases/src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,12 @@
+//La creación de este componente ha sido de manera automatica desde la consola
+//con los comandos ng g c heroes/listado
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent  { 
+
+}
