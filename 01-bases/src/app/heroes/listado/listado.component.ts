4fc9057cdeb326486c0d49343af0ae2d0de84ba3
3bort <78028202+3bort@users.noreply.github.com>
2021-02-17 22:20:41 +0100

    Directiva ngIf
    Condición if dentro del fichero html

--- 01-bases/src/app/heroes/listado/listado.component.ts
+++ 01-bases/src/app/heroes/listado/listado.component.ts
@@ -11,11 +11,13 @@ export class ListadoComponent  {
 
   heroes: string[] = ['Spiderman','Ironman','Hulk','Daredevil'];
   borrado: string = "";
+  esBorrado = false;
 
   borrarHeroe(){
     console.log("Borrando...");
     const heroeBorrado = this.heroes.shift() || ""; //Borra el primer elemento (con control + espacio vemos la descripción de los metodos)
     this.borrado = heroeBorrado;
+    this.esBorrado = true;
   }
 
 }
